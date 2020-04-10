ebbe439c5ac44d48a76eb7f3f39ff3465c2ebefd
unknown <Vanya.Goudz@gmail.com>
2020-04-10 21:12:56 +0300

    41517

--- homework-1/task-4.js
+++ homework-1/task-4.js
@@ -1,1 +1,21 @@
-"use strict";
+// "use strict";
+
+// const total = 100;
+
+// const ordered = 50;
+
+// let total1 = 20;
+// let ordered1 = 19;
+// if (ordered1 > total1) {
+//   console.log("На складе недостаточно товаров");
+// } else {
+//   console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
+
+// let total2 = 20;
+// let ordered2 = 21;
+// if (ordered2 > total2) {
+//   console.log("На складе недостаточно товаров");
+// } else {
+//   console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
--- homework-1/task-6.js
+++ homework-1/task-6.js
@@ -1,1 +1,27 @@
-"use strict";
+// "use strict";
+
+// let credits = 23580;
+// let pricePerDroid = 3000;
+
+// let wantToBuy = prompt("Укажите количество дроидов");
+// let totalPrice;
+// // console.log(wantToBuy);
+
+// if (wantToBuy === null) {
+//   console.log("Отменено пользователем!");
+// } else {
+//   totalPrice = pricePerDroid * Number(wantToBuy);
+//   //   console.log(wantToBuy);
+//   //   console.log(totalPrice);
+//   if (totalPrice > credits) {
+//     console.log("Недостаточно средств на счету!");
+//   } else {
+//     console.log(
+//       `Вы купили ${wantToBuy} дроидов, на счету осталось ${credits -
+//         totalPrice} кредитов.`
+//     );
+//   }
+// }
+
+// let count = Math.floor(credits / pricePerDroid);
+// console.log(count);
