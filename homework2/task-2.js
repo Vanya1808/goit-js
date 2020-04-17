244b517a2bb4eb188ab8566f68d276fd887e9b60
Vanya1808 <56477758+Vanya1808@users.noreply.github.com>
2020-04-17 19:33:45 +0300

    Add files via upload

--- homework2/task-2.js
+++ homework2/task-2.js
@@ -4,7 +4,7 @@
 // let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
 
 // for (let a = 0; a < test1.length; a += 1) {
-//   if (test1[a] <= 12) {
+//   if (test1[a] <= score1) {
 //     continue;
 //   }
 //   console.log(`This score is okey: ${test1[a]}`); 
@@ -16,7 +16,7 @@
 // let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
 
 // for (let a = 0; a < test2.length; a += 1) {
-//   if (test2[a] <= 15) {
+//   if (test2[a] <= score2) {
 //     continue;
 //   }
 //   console.log(`This score is okey: ${test2[a]}`);  
@@ -28,7 +28,7 @@
 // let test3 = [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
 
 // for (let a = 0; a < test3.length; a += 1) {
-//   if (test3[a] <= 16) {
+//   if (test3[a] <= score3) {
 //     continue;
 //   }
 //   console.log(`This score is okey: ${test3[a]}`);  
