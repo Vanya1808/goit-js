ae789c8f7c3baf4c727d94ebac994efbde4ec33c
Vanya1808 <56477758+Vanya1808@users.noreply.github.com>
2020-04-17 19:54:42 +0300

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// let score1 = 12;
+// let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
+
+// for (let a = 0; a < test1.length; a += 1) {
+//   if (test1[a] <= score1) {
+//     continue;
+//   }
+//   console.log(`This score is okey: ${test1[a]}`); 
+// }
+
+// 3 
+
+// let score2;
+// let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+
+// for (let a = 0; a < test2.length; a += 1) {
+//   if (test2[a] <= score2) {
+//     continue;
+//   }
+//   console.log(`This score is okey: ${test2[a]}`);  
+// }
+
+// 4
+
+// let score3;
+// let test3 = [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+
+// for (let a = 0; a < test3.length; a += 1) {
+//   if (test3[a] <= scorez) {
+//     continue;
+//   }
+//   console.log(`This score is okey: ${test3[a]}`);  
+// }
+
+// 5
+
+// let tests = [
+//     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+//     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+//     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
+// ]
+
+// for (let i = 0; i < tests.length; i += 1) {
+//     console.log(tests[i]);    
+//  for (let j = 0; j < tests[i].length; j += 1) {
+//    console.log(tests[i][j]);//
+//   }
+//   }
