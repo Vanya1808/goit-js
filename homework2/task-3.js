f103ba96ae2ed152b1b97b8e4d3884a7bf569b88
Vanya1808 <56477758+Vanya1808@users.noreply.github.com>
2020-05-08 20:49:58 +0300

    Add files via upload

---
+++ homework2/task-3.js
@@ -0,0 +1,71 @@
+// // 1
+// let authors = ['William Shakespeare', 'Guy de Maupassant', 'Dante Alighieri', 'Mikhail Bulgakov', 'Albert Camus', 'Erich Maria Remarque'];
+
+// authors[6] = 'Honore de Balzac';
+// authors[7] = 'Miguel de Cervantes';
+// authors[8] = 'Anton Chekhov';
+
+// authors.unshift("Gabriel Garcia Marquez");
+
+// authors.unshift("Edgar Allan Poe");
+
+// authors.unshift("Charles Dickens");
+
+// console.log(authors);
+
+// // 2
+
+// let score1 = 12;
+// let test1 = [12, 32, 15, 17, 3, 4, 98, 67, 532, 611, 7, 100, 42];
+
+// for (let a = 0; a < test1.length; a += 1) {
+//     if(test1[a] > score1){
+//         console.log(test1[a]);
+//     }
+//     else{
+//         continue;
+//     }
+
+// }
+
+// 3
+
+// let score2 = 15;
+// let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+
+// for (let a = 0; a < test2.length; a += 1) {
+//     if(test2[a] > score2){
+//         console.log(test2[a]);
+//     }
+//     else{
+//         continue;
+//     }
+
+// }
+
+// 4
+
+// let score3 = 16;
+// let test3 = [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+
+// for (let a = 0; a < test3.length; a += 1) {
+//     if(tes32[a] > score3){
+//         console.log(test3[a]);
+//     }
+//     else{
+//         continue;
+//     }
+
+// }
+
+// 5 
+
+// let tests = [
+//     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+//     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+//     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
+// ]
+
+// for (let a = 0; a < tests[0].length; a += 1) {
+//     console.log(tests[a]);    
+// }
