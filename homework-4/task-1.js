66fee69b8297a0c27b486e1a45dda8d302152d6e
Vanya1808 <56477758+Vanya1808@users.noreply.github.com>
2020-06-03 14:43:50 +0300

    Add files via upload

--- homework-4/task-1.js
+++ homework-4/task-1.js
@@ -1,109 +1,95 @@
 "use strict";
 
-const account= {
+// 1
 
-    owner: 'Mango',
-  
-    balance: 24000,
-  
-    discount: 0.1,
-  
-    orders: ['order-1', 'order-2', 'order-3'],
-  
-    changeDiscount(value) {
-  
-      this.discount = value;
-  
-    },
-  
-    showOrders() {
-  
-      return this.orders;
-  
-    },
-  
-    addOrder(cost, order) {
-  
-      this.balance -= cost;
-  
-      this.orders.push(order);
-  
-    },
-  
-  };
-  
-   
-  
-  account.changeDiscount(0.15);
-  
-  console.log(account.discount); // 0.15
-  
-  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+const user = {
+    name: "Semen",
+    age: 15,
+    hobby: "html",
+    premium: true,
+};
 
-  account.addOrder(5000, 'order-4');
-  
-  console.log(account.balance); // 19000
-  
-  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+user.mood = happy;
+user.hobby = coding;
+user.premium = false;
 
-  // 2 
+const toShowUpdatedUser = function (obj) {
+  let save = Object.keys(obj);
+  for (let key of save) {
+    console.log(`${key}: ${obj[key]}`);
+  }
+};
 
-  const inventory= {
+toShowUpdatedUser(user);
 
-    items: ['Knife', 'Gas mask'],
+// 2
+
+function countProps(obj) {
+    return Object.keys(obj).length;
+}
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
+
+// 3
+const countTotalSalary = function (employees) {
+  let sum = 0;
+  let arrayOfValues = Object.values(employees);
+  for (let cal of arrayOfValues) {
+    sum += cal;
+  }
+  return sum;
+};
+
+console.log(
+
+    countTotalSalary({
   
-    add(itemName) {
+      mango: 100,
   
-      console.log(`Adding ${itemName} to inventory`);
+      poly: 150,
   
-   
+      alfred: 80,
   
-      inventory.items.push(itemName);
+    })
   
-    },
+  ); // 330
   
-    remove(itemName) {
+  console.log(
   
-      console.log(`Removing ${itemName} from inventory`);
+    countTotalSalary({
   
-   
+      kiwi: 200,
   
-      inventory.items =inventory.items.filter(item => item !== itemName);
+      lux: 50,
   
-    },
+      chelsy: 150,
   
-  };
+    })
   
-   
-  
-  const invokeInventoryAction=function(itemName, action) {
-  
-    console.log(`Invoking action on ${itemName}`);
-  
-    action(itemName);
-  
-  };
-  
-   
-  
-  invokeInventoryAction('Medkit', inventory.add);
-  
-  // Invoking action on Medkit
-  
-  // Adding Medkit to inventory
-  
-   
-  
-  console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-  
-   
-  
-  invokeInventoryAction('Gas mask', inventory.remove);
-  
-  // Invoking action on Gas mask
-  
-  // Removing Gas mask from inventory
-  
-   
-  
-  console.log(inventory.items); // ['Knife', 'Medkit']
+  ); // 400
+
+// 4
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function (allProdcuts, productName) {
+let money ;
+for (let i = 0; i < allProdcuts.length; i+= 1) {
+
+  let values = Object.values(allProdcuts[i]);
+        if (values[0] === productName) {
+            money = values[1] * values[2];
+            return money;
+        };
+    };
+
+    console.log(calculateTotalPrice(products, "Радар")); // 5200
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
